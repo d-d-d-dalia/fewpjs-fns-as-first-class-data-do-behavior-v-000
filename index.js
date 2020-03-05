fd15189d7c52a3012bdc5c884fd912646dbe3ae3
dalia sawaya <dalia.sawaya@flatironschool.com>
2020-03-05 15:59:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function greet(string) {
   const hours = string.split(":")
   if (parseInt(hours[0]) < 12) {
     return "Good Morning"
-  else if (parseInt(hours[0].between ))
+  else if (parseInt(hours[0] >= 12) )
   }
 }
 
